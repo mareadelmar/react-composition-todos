e42adb3b61129a40e805c233fdcfd4a10edf8e9e
mareadelmar <mar.ortega.v@gmail.com>
2021-12-03 12:54:28 -0300

    Project base + composition of TodoHeader

---
+++ src/TodoCounter/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TodoCounter.css";
+
+function TodoCounter({ completedTodos, totalTodos }) {
+	return (
+		<h2 className='TodoCounter'>
+			Has completado {completedTodos} de {totalTodos} TODOs
+		</h2>
+	);
+}
+
+export { TodoCounter };
