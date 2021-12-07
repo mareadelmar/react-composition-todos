077caee2d43ed7ce06eea19d97fc1b2856bab600
mareadelmar <mar.ortega.v@gmail.com>
2021-12-07 09:19:34 -0300

    Change TodoList's properties to render props

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import './TodoList.css'
+import React from "react";
+import "./TodoList.css";
 
 function TodoList(props) {
-  return (
-    <section>
-      <ul>
-        {props.children}
-      </ul>
-    </section>
-  );
+	return (
+		<section>
+			{props.error && props.onError()}
+			{props.loading && props.onLoading()}
+
+			{!props.loading &&
+				!props.searchedTodos.length &&
+				props.onEmptyTodos()}
+
+			<ul>{props.searchedTodos.map(todo => props.render(todo))}</ul>
+		</section>
+	);
 }
 
 export { TodoList };
